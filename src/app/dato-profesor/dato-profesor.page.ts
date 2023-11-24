4481ec97db15594540cb4627f5970928c5b5ecd3
sebathemaker <se.parraguez@duocuc.cl>
2023-11-24 00:44:12 -0400

    ARREGLO ERRORES

--- src/app/dato-profesor/dato-profesor.page.ts
+++ src/app/dato-profesor/dato-profesor.page.ts
@@ -9,33 +9,16 @@ import { SupabaseService } from '../Service/supabase.service';
   styleUrls: ['./dato-profesor.page.scss'],
 })
 export class DatoProfesorPage implements OnInit {
-  profesor: Profesor | null = null;
-  profesorId: number;
+  profesorActual: Profesor | null = null;
 
-  constructor(
-    private route: ActivatedRoute,
-    private supabaseService: SupabaseService,
-  ) {
-    this.profesorId = this.route.snapshot.params['profesorId'];
-  }
-
+  constructor(private supabaseService: SupabaseService) { }
   ngOnInit() {
-    this.loadProfesorData();
-  }
 
-  private loadProfesorData(): void {
-    this.supabaseService.getProfesorById(this.profesorId).subscribe(
-      (profesor: Profesor) => {
-        this.profesor = profesor;
-      },
-      (error) => {
-        console.error(
-          'Error al conseguir el id del profesor ',
-          this.profesorId,
-          ' Error: ',
-          error
-        );
-      }
-    );
-  }
+    if (this.supabaseService.getProfesorActual()) {
+
+      this.profesorActual = this.supabaseService.getProfesorActual();
+    } else {
+
+    }
+    }
 }
